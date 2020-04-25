e8b622dcf8c6906931d6aeef6d219206c83e9fae
Dev Don <devdon1@outlook.com>
2020-04-25 15:36:45 +0200

    Add project files.

---
+++ FirstPWA/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstPWA.Models;
+using FirstPWA.Services;
+
+namespace FirstPWA.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IPasswordService _passwordService;
+
+        public HomeController(ILogger<HomeController> logger, IPasswordService passwordService)
+        {
+            _logger = logger;
+            _passwordService = passwordService;
+        }
+
+        public IActionResult Index()
+        {
+            Password password = new Password()
+            {
+                PasswordString = ""
+            };
+
+            return View("Index", password);
+        }
+
+        public IActionResult GetPassword(int passwordLength)
+        {
+            Password password = new Password()
+            {
+                PasswordString = _passwordService.GeneratePassword(passwordLength)
+            };
+
+            return View("Index", password);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FirstPWA/Services/PasswordService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstPWA.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public string GeneratePassword(int passwordLength)
+        {
+            char[] chars = GeneratePasswordCharacters();
+
+            string[] password = new string[passwordLength];
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                password[i] = GetRandomCharacter(chars);
+            }
+
+            return String.Concat(password);
+        }
+
+        private char[] GeneratePasswordCharacters()
+        {
+            char[] passwordChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()-+={}<>[]/?.,".ToCharArray();
+
+            return passwordChars;
+        }
+
+        private string GetRandomCharacter(char[] array)
+        {
+            Random random = new Random();
+            string character = array.GetValue(random.Next(0, array.Length)).ToString();
+
+            return character;
+        }
+    }
+}
+
